3f869feb3c822897968bd7631da2faf2002314d6
Elvis Dragon Mao <info@elvismao.com>
2026-05-07 13:12:38 +0800

    chore: reset docs astro project

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,27 +1,5 @@
 // @ts-check
-import starlight from "@astrojs/starlight";
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	integrations: [
-		starlight({
-			title: "My Docs",
-			social: [{ icon: "github", label: "GitHub", href: "https://github.com/withastro/starlight" }],
-			customCss: ["./src/styles/custom.css"],
-			sidebar: [
-				{
-					label: "Guides",
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: "Example Guide", slug: "guides/example" }
-					]
-				},
-				{
-					label: "Reference",
-					autogenerate: { directory: "reference" }
-				}
-			]
-		})
-	]
-});
+export default defineConfig({});
